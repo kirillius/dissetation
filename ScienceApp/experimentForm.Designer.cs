9e82444fd8fd712357c88855bc444e15e173be52
Kirill Lavrentev <klavrentev@mail.ru>
2018-01-22 22:15:29 +1000

    запросы к серверу

--- ScienceApp/experimentForm.Designer.cs
+++ ScienceApp/experimentForm.Designer.cs
@@ -31,12 +31,16 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.nameDictionary = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.countRequest = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameTable = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.startButton = new System.Windows.Forms.Button();
             this.clearButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameTable1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -48,10 +52,12 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nameDictionary,
-            this.countRequest});
-            this.dataGridView1.Location = new System.Drawing.Point(387, 12);
+            this.countRequest,
+            this.nameTable});
+            this.dataGridView1.Location = new System.Drawing.Point(516, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(288, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(384, 185);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
             // 
@@ -67,11 +73,18 @@
             this.countRequest.HeaderText = "Количество запросов";
             this.countRequest.Name = "countRequest";
             // 
+            // nameTable
+            // 
+            this.nameTable.HeaderText = "nameTable";
+            this.nameTable.Name = "nameTable";
+            this.nameTable.Visible = false;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(306, 23);
+            this.button1.Location = new System.Drawing.Point(408, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = ">>";
             this.button1.UseVisualStyleBackColor = true;
@@ -79,9 +92,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(306, 52);
+            this.button2.Location = new System.Drawing.Point(408, 64);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "<<";
             this.button2.UseVisualStyleBackColor = true;
@@ -93,12 +107,53 @@
             this.dataGridView2.AllowUserToDeleteRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.name});
-            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.name,
+            this.nameTable1});
+            this.dataGridView2.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
-            this.dataGridView2.Size = new System.Drawing.Size(288, 150);
+            this.dataGridView2.Size = new System.Drawing.Size(384, 185);
             this.dataGridView2.TabIndex = 3;
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(695, 238);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 28);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(803, 238);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 28);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 305);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(884, 171);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Лог:";
             // 
             // name
             // 
@@ -107,31 +162,20 @@
             this.name.Name = "name";
             this.name.ReadOnly = true;
             // 
-            // startButton
+            // nameTable1
             // 
-            this.startButton.Enabled = false;
-            this.startButton.Location = new System.Drawing.Point(521, 193);
-            this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(75, 23);
-            this.startButton.TabIndex = 4;
-            this.startButton.Text = "Старт";
-            this.startButton.UseVisualStyleBackColor = true;
-            this.startButton.Click += new System.EventHandler(this.startButton_Click);
-            // 
-            // clearButton
-            // 
-            this.clearButton.Location = new System.Drawing.Point(602, 193);
-            this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(75, 23);
-            this.clearButton.TabIndex = 5;
-            this.clearButton.Text = "Очистить";
-            this.clearButton.UseVisualStyleBackColor = true;
+            this.nameTable1.HeaderText = "nameTable1";
+            this.nameTable1.Name = "nameTable1";
+            this.nameTable1.ReadOnly = true;
+            this.nameTable1.Visible = false;
             // 
             // experimentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 224);
+            this.ClientSize = new System.Drawing.Size(919, 488);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.dataGridView2);
@@ -139,6 +183,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "experimentForm";
             this.Text = "Новый эксперимент";
@@ -146,6 +191,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,8 +203,12 @@
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Button clearButton;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridViewTextBoxColumn nameDictionary;
         private System.Windows.Forms.DataGridViewTextBoxColumn countRequest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameTable1;
     }
 }
--- ScienceApp/experimentForm.cs
+++ ScienceApp/experimentForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,9 +15,13 @@ namespace ScienceApp
     public partial class experimentForm : Form
     {
         dissertationEntities context = new dissertationEntities();
+        List<Dictionary<String, String>> taskRequest;
+        string connectionString;
+
         public experimentForm()
         {
             InitializeComponent();
+            connectionString = "Server=185.158.153.126;Database=dissertation;User Id=rental;Password=kirillius1991;";
         }
 
         private void experimentForm_Load(object sender, EventArgs e)
@@ -24,7 +29,7 @@ namespace ScienceApp
             List<dictionaries> listDictionaries = context.dictionaries.ToList(); 
             foreach(var item in listDictionaries)
             {
-                dataGridView2.Rows.Add(item.displayName);
+                dataGridView2.Rows.Add(item.displayName, item.nameTable);
             }
         }
 
@@ -33,7 +38,7 @@ namespace ScienceApp
             var currentRow = dataGridView2.CurrentRow;
             if(currentRow!=null)
             {
-                dataGridView1.Rows.Add(dataGridView2.Rows[currentRow.Index].Cells[0].Value);
+                dataGridView1.Rows.Add(dataGridView2.Rows[currentRow.Index].Cells[0].Value, 0, dataGridView2.Rows[currentRow.Index].Cells[1].Value);
                 dataGridView2.Rows.Remove(currentRow);
             }
         }
@@ -43,7 +48,7 @@ namespace ScienceApp
             var currentRow = dataGridView1.CurrentRow;
             if (currentRow != null)
             {
-                dataGridView2.Rows.Add(dataGridView1.Rows[currentRow.Index].Cells[0].Value);
+                dataGridView2.Rows.Add(dataGridView1.Rows[currentRow.Index].Cells[0].Value, dataGridView1.Rows[currentRow.Index].Cells[2].Value);
                 dataGridView1.Rows.Remove(currentRow);
             }
         }
@@ -61,11 +66,55 @@ namespace ScienceApp
         private void startButton_Click(object sender, EventArgs e)
         {
             String parametersRequest = "";
-            foreach (DataGridViewRow row in dataGridView2.Rows)
+            taskRequest = new List<Dictionary<string, string>>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
             {
                 parametersRequest += ("Справочник: " + row.Cells[0].Value + ", количество запросов: " + row.Cells[1].Value + "\n");
+
+                Dictionary<String, String> item = new Dictionary<string, string>();
+                item.Add("nameTable", row.Cells[2].Value.ToString());
+                item.Add("countRequest", row.Cells[1].Value.ToString());
+                taskRequest.Add(item);
             }
-            MessageBox.Show(parametersRequest);
+            richTextBox1.Text = "Начата отправка запросов на сервер БД со следующей конфигурацией:\n" + parametersRequest;
+            executeTaskRequest();
+        }
+
+        private void executeTaskRequest()
+        {
+            if (taskRequest == null)
+                return;
+
+            //берем конфиг запросов, делаем
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                foreach (var item in taskRequest)
+                {
+                    string queryString = "SELECT * FROM [dbo].[mos]";
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            String text = String.Format("Полученная запись: {0}, {1}", reader["id"], reader["name"]);
+                            richTextBox1.Text += "\n" + text;
+                        }
+                    }
+                    catch(Exception exp)
+                    {
+                        String text = String.Format("Ошибка: {0}", exp.Message);
+                        richTextBox1.Text += "\n" + text;
+                    }
+                    finally
+                    {
+                        // Always call Close when done reading.
+                        reader.Close();
+                        connection.Close();
+                    }
+                }
+            }
         }
     }
 }
